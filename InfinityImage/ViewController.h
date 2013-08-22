2560fd4727e0b9895e3a09d01c81e0291d160e1e
Ivelin Ivanov <ivelin.ivanov@Student01s-Mac-mini.local>
2013-08-22 16:44:26 +0300

    Initial Commit

---
+++ InfinityImage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  InfinityImage
+//
+//  Created by Ivelin Ivanov on 8/22/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ InfinityImage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  InfinityImage
+//
+//  Created by Ivelin Ivanov on 8/22/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
